3c743d7c579e93ed283630ced6bdea198fe9959f
ofcyln <ofcyln@gmail.com>
2020-01-15 09:35:40 +0100

    Element selections for future usage

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pageElements = {
+        sceneBackground: () => document.querySelector('.background-area'),
+        sceneLogo: () => document.querySelector('.logo-area'),
+        sceneBrandName: () => document.querySelector('.brand-name-area'),
+        backgroundColorInput: () => document.querySelector('#backgroundColor'),
+        upDownLogoPositionInput: () => document.querySelector('#updDown'),
+        leftRightLogoPositionInput: () => document.querySelector('#leftRight'),
+        brandNameInput: () => document.querySelector('#brandName'),
+        fontTypeInput: () => document.querySelector('#fontTypeSelect'),
+        fontSizeInput: () => document.querySelector('#fontSize'),
+        upDownFontPositionInput: () => document.querySelector('#updDownFont'),
+        leftRightFontPositionInput: () => document.querySelector('#leftRightFont'),
+        scaleLogoInput: () => document.querySelector('#scaleLogo'),
+    };
+
+    
+
+    console.log();
+});
