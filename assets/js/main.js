bbc2f69417f45f88ab3eef137e5e08a579ec51df
ofcyln <ofcyln@gmail.com>
2020-01-15 10:53:17 +0100

    Logo element position calculations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const pageElements = {
+        scene: () => document.querySelector('.logo-scene-container'),
         sceneBackground: () => document.querySelector('.background-area'),
         sceneLogo: () => document.querySelector('.logo-area'),
         sceneBrandName: () => document.querySelector('.brand-name-area'),
@@ -14,5 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
         scaleLogoInput: () => document.querySelector('#scaleLogo'),
     };
 
-    
+    const calculateOneToHundredValueOfScene = () => ([pageElements.scene().getBoundingClientRect().width / 100, pageElements.scene().getBoundingClientRect().height / 100])
+    const [sceneWidth, sceneHeight] = calculateOneToHundredValueOfScene();
+
+    // const eventListenerForInputChanges = (element, func) => element.addEventListener('input', func);
+
+    // Object.keys(pageElements)
+    // .filter(element => element.includes('Input'))
+    // .forEach(inputElement => {
+    //     eventListenerForInputChanges(
+    //         pageElements[inputElement], 
+    //         () => console.log(pageElements[inputElement].value
+    //             ));
+    // });
+
+    pageElements.upDownLogoPositionInput()
+    .addEventListener(
+        'input', 
+        () => {
+            pageElements.sceneLogo().style.top = `${(sceneHeight * pageElements.upDownLogoPositionInput().value)}px`;
+        },
+
+    );
 });
